5681e33b679e7e4cfd241a02272c7e8015ada18a
Haoquan Guan <howardghq@outlook.com>
2023-05-31 13:32:29 +0800

    minor revision

--- prediction24.py
+++ prediction24.py
@@ -136,155 +136,156 @@ test_dataset = PatientDataset("./dataset/test_clean"+str(FIX_LEN)+".csv")
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-choice = "RNN"
+choices = ["RNN", "GRU", "LSTM"]
 
-if choice == "RNN":
-    model = RNNModel(input_size, hidden_size, output_size)
-elif choice == "LSTM":
-    model = LSTMModel(input_size, hidden_size, output_size)
-elif choice == "GRU":
-    model = GRUModel(input_size, hidden_size, output_size)
-# model = TransformerModel(input_size, hidden_size, num_layers, num_heads, output_size, dropout)
+for choice in choices:
+    if choice == "RNN":
+        model = RNNModel(input_size, hidden_size, output_size)
+    elif choice == "LSTM":
+        model = LSTMModel(input_size, hidden_size, output_size)
+    elif choice == "GRU":
+        model = GRUModel(input_size, hidden_size, output_size)
+    # model = TransformerModel(input_size, hidden_size, num_layers, num_heads, output_size, dropout)
 
-criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-train_losses = []
-train_f1s = []
-train_accuracies = []
-test_losses = []
-test_f1s = []
-test_accuracies = []
+    train_losses = []
+    train_f1s = []
+    train_accuracies = []
+    test_losses = []
+    test_f1s = []
+    test_accuracies = []
 
-for epoch in range(num_epochs):
-    model.train()
-    train_loss = 0.0
-    train_samples = 0
-    train_label = np.array([])
-    train_pred = np.array([])
-    for batch in train_loader:
-        # print('batch:\t',batch)
-        inputs = batch['inputs']
-        labels = batch['label'].reshape(-1)
-        # if len(labels)<2:
-        #     print('inputs:\t',inputs)
-        # print('labels:\t',labels)
-        # print('inputs_shape:\t',inputs.shape)
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0.0
+        train_samples = 0
+        train_label = np.array([])
+        train_pred = np.array([])
+        for batch in train_loader:
+            # print('batch:\t',batch)
+            inputs = batch['inputs']
+            labels = batch['label'].reshape(-1)
+            # if len(labels)<2:
+            #     print('inputs:\t',inputs)
+            # print('labels:\t',labels)
+            # print('inputs_shape:\t',inputs.shape)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        tmp = model(inputs)
-        outputs = tmp.reshape(-1) if len(tmp)==len(labels) else tmp[-1].reshape(-1)
-        # print('model(inputs):\t',tmp)
-        # print('model(inputs)_shape:\t',tmp.shape)
-        # print('model(inputs)[-1]:\t',tmp[-1])
-        # print('model(inputs)[-1]_shape:\t',tmp[-1].shape)
-        # print('outputs:\t',outputs)
-        # print('outputs_shape:\t',outputs.shape)
-        # print('labels:\t',labels)
-        # print('labels_shape:\t',labels.shape)
+            tmp = model(inputs)
+            outputs = tmp.reshape(-1) if len(tmp)==len(labels) else tmp[-1].reshape(-1)
+            # print('model(inputs):\t',tmp)
+            # print('model(inputs)_shape:\t',tmp.shape)
+            # print('model(inputs)[-1]:\t',tmp[-1])
+            # print('model(inputs)[-1]_shape:\t',tmp[-1].shape)
+            # print('outputs:\t',outputs)
+            # print('outputs_shape:\t',outputs.shape)
+            # print('labels:\t',labels)
+            # print('labels_shape:\t',labels.shape)
 
-        loss = criterion(outputs, labels)
-        # print(loss)
-        train_loss += loss.item()
-        train_samples += 1
+            loss = criterion(outputs, labels)
+            # print(loss)
+            train_loss += loss.item()
+            train_samples += 1
 
-        for id in range(len(outputs)):
-            if int(labels[id].item()) == 0:
-                train_label = np.append(train_label, 0)
-            else:
-                train_label = np.append(train_label, 1)
-            if outputs[id].item() < 0.5:
-                train_pred = np.append(train_pred, 0)
-            else:
-                train_pred = np.append(train_pred, 1)
+            for id in range(len(outputs)):
+                if int(labels[id].item()) == 0:
+                    train_label = np.append(train_label, 0)
+                else:
+                    train_label = np.append(train_label, 1)
+                if outputs[id].item() < 0.5:
+                    train_pred = np.append(train_pred, 0)
+                else:
+                    train_pred = np.append(train_pred, 1)
 
-        loss.backward()
-        optimizer.step()
+            loss.backward()
+            optimizer.step()
 
-    average_loss = train_loss / train_samples
-    print(f"Epoch {epoch+1}/{num_epochs}, \tTrain Loss: {average_loss:.4f}, Accuracy: {accuracy_score(train_label, train_pred):.4f}, Precision: {precision_score(train_label, train_pred):.4f}, Recall: {recall_score(train_label, train_pred):.4f}, F1: {f1_score(train_label, train_pred):.4f}")
-    train_losses.append(average_loss)
-    train_f1s.append(f1_score(train_label, train_pred))
-    train_accuracies.append(accuracy_score(train_label, train_pred))
+        average_loss = train_loss / train_samples
+        print(f"Epoch {epoch+1}/{num_epochs}, \tTrain Loss: {average_loss:.4f}, Accuracy: {accuracy_score(train_label, train_pred):.4f}, Precision: {precision_score(train_label, train_pred):.4f}, Recall: {recall_score(train_label, train_pred):.4f}, F1: {f1_score(train_label, train_pred):.4f}")
+        train_losses.append(average_loss)
+        train_f1s.append(f1_score(train_label, train_pred))
+        train_accuracies.append(accuracy_score(train_label, train_pred))
 
-    model.eval()
-    test_loss = 0.0
-    test_samples = 0
-    test_label = np.array([])
-    test_pred = np.array([])
-    for batch in test_loader:
-        inputs = batch['inputs']
-        labels = batch['label'].reshape(-1)
+        model.eval()
+        test_loss = 0.0
+        test_samples = 0
+        test_label = np.array([])
+        test_pred = np.array([])
+        for batch in test_loader:
+            inputs = batch['inputs']
+            labels = batch['label'].reshape(-1)
 
-        tmp = model(inputs)
-        outputs = tmp.reshape(-1) if len(tmp)==len(labels) else tmp[-1].reshape(-1)
+            tmp = model(inputs)
+            outputs = tmp.reshape(-1) if len(tmp)==len(labels) else tmp[-1].reshape(-1)
 
-        loss = criterion(outputs, labels)
-        test_loss += loss.item()
-        test_samples += 1
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+            test_samples += 1
 
-        for id in range(len(outputs)):
-            if int(labels[id].item()) == 0:
-                test_label = np.append(test_label, 0)
-            else:
-                test_label = np.append(test_label, 1)
-            if outputs[id].item() < 0.5:
-                test_pred = np.append(test_pred, 0)
-            else:
-                test_pred = np.append(test_pred, 1)
+            for id in range(len(outputs)):
+                if int(labels[id].item()) == 0:
+                    test_label = np.append(test_label, 0)
+                else:
+                    test_label = np.append(test_label, 1)
+                if outputs[id].item() < 0.5:
+                    test_pred = np.append(test_pred, 0)
+                else:
+                    test_pred = np.append(test_pred, 1)
 
-    average_loss = test_loss / test_samples
-    print(f"\t\t\tTest Loss: {average_loss:.4f}, Accuracy: {accuracy_score(test_label, test_pred):.4f}, Precision: {precision_score(test_label, test_pred):.4f}, Recall: {recall_score(test_label, test_pred):.4f}, F1: {f1_score(test_label, test_pred):.4f}")
-    test_losses.append(average_loss)
-    test_f1s.append(f1_score(test_label, test_pred))
-    test_accuracies.append(accuracy_score(test_label, test_pred))
+        average_loss = test_loss / test_samples
+        print(f"\t\t\tTest Loss: {average_loss:.4f}, Accuracy: {accuracy_score(test_label, test_pred):.4f}, Precision: {precision_score(test_label, test_pred):.4f}, Recall: {recall_score(test_label, test_pred):.4f}, F1: {f1_score(test_label, test_pred):.4f}")
+        test_losses.append(average_loss)
+        test_f1s.append(f1_score(test_label, test_pred))
+        test_accuracies.append(accuracy_score(test_label, test_pred))
 
-    if epoch == num_epochs-1:
-        fpr, tpr, thresholds = roc_curve(test_label, test_pred)
+        if epoch == num_epochs-1:
+            fpr, tpr, thresholds = roc_curve(test_label, test_pred)
 
-        plt.figure(figsize=(9, 6))
-        plt.plot(fpr, tpr)
-        plt.xlabel('FPR')
-        plt.ylabel('TPR')
-        plt.title('ROC')
-        plt.savefig("../figure/" + choice+ "_ROC.jpg")
+            plt.figure(figsize=(9, 6))
+            plt.plot(fpr, tpr)
+            plt.xlabel('FPR')
+            plt.ylabel('TPR')
+            plt.title('ROC')
+            plt.savefig("../figure/" + choice+ "_ROC.jpg")
 
-        confusion = confusion_matrix(test_label, test_pred)
+            confusion = confusion_matrix(test_label, test_pred)
 
-        plt.figure(figsize=(9, 6))
-        plt.imshow(confusion, interpolation='nearest', cmap=plt.confusion.Blues)
-        plt.title('Confusion Matrix')
-        plt.colorbar()
-        plt.xticks([0, 1])
-        plt.yticks([0, 1])
-        plt.xlabel('Predicted Condition')
-        plt.ylabel('Actual Condition')
-        plt.savefig("../figure/" + choice+"_Confusion.jpg")
+            plt.figure(figsize=(9, 6))
+            plt.imshow(confusion, interpolation='nearest', cmap=plt.confusion.Blues)
+            plt.title('Confusion Matrix')
+            plt.colorbar()
+            plt.xticks([0, 1])
+            plt.yticks([0, 1])
+            plt.xlabel('Predicted Condition')
+            plt.ylabel('Actual Condition')
+            plt.savefig("../figure/" + choice+"_Confusion.jpg")
 
-plt.figure(figsize=(9, 6))
-plt.plot(list(range(1, num_epochs + 1)), train_accuracies, label="train")
-plt.plot(list(range(1, num_epochs + 1)), test_accuracies, label="test")
-plt.legend(loc=4)
-plt.title(choice)
-plt.xlabel("Epoch")
-plt.ylabel("Accuracy")
-plt.savefig("../figure/" + choice + "_Accuracy.jpg", bbox_inches='tight')
+    plt.figure(figsize=(9, 6))
+    plt.plot(list(range(1, num_epochs + 1)), train_accuracies, label="train")
+    plt.plot(list(range(1, num_epochs + 1)), test_accuracies, label="test")
+    plt.legend(loc=4)
+    plt.title(choice)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.savefig("../figure/" + choice + "_Accuracy.jpg", bbox_inches='tight')
 
-plt.figure(figsize=(9, 6))
-plt.plot(list(range(1, num_epochs + 1)), train_f1s, label="train")
-plt.plot(list(range(1, num_epochs + 1)), test_f1s, label="test")
-plt.legend(loc=4)
-plt.title(choice)
-plt.xlabel("Epoch")
-plt.ylabel("F1 Score")
-plt.savefig("../figure/" + choice + "_F1.jpg", bbox_inches='tight')
+    plt.figure(figsize=(9, 6))
+    plt.plot(list(range(1, num_epochs + 1)), train_f1s, label="train")
+    plt.plot(list(range(1, num_epochs + 1)), test_f1s, label="test")
+    plt.legend(loc=4)
+    plt.title(choice)
+    plt.xlabel("Epoch")
+    plt.ylabel("F1 Score")
+    plt.savefig("../figure/" + choice + "_F1.jpg", bbox_inches='tight')
 
-plt.figure(figsize=(9, 6))
-plt.plot(list(range(1, num_epochs + 1)), train_losses, label="train")
-plt.plot(list(range(1, num_epochs + 1)), test_losses, label="test")
-plt.legend(loc=4)
-plt.title(choice)
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.savefig("../figure/" + choice + "_Loss.jpg", bbox_inches='tight')
+    plt.figure(figsize=(9, 6))
+    plt.plot(list(range(1, num_epochs + 1)), train_losses, label="train")
+    plt.plot(list(range(1, num_epochs + 1)), test_losses, label="test")
+    plt.legend(loc=4)
+    plt.title(choice)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.savefig("../figure/" + choice + "_Loss.jpg", bbox_inches='tight')
